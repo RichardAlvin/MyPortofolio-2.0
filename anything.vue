b457b99d38e619d16bb19a3b9c0723656a4ce68f
Richard Alvin <richardalvinpratama8@gmail.com>
2023-08-06 23:02:43 +0700

    initialize

---
+++ anything.vue
@@ -0,0 +1,17 @@
+<template>
+    <a href="#" @click="view('home')">Home</a>
+    <a href="#" @click="view('counter')">Counter</a>
+
+    <NuxtPage />
+
+    <lazyHomeWelcome v-if="page == 'home'"/>
+    <lazyCounter v-if="page == 'counter'"/>
+</template>
+
+<script setup>
+const page = ref("home");
+
+function view(name){
+    page.value = name;
+}
+</script>
